fbaa164a79cb5a04026b9ca4104a84a91cfdcc1a
Aidan Oldfield <aidanoldfield12@gmail.com>
2024-10-04 13:20:25 +0100

    Added error handling

--- Name_fetch.py
+++ Name_fetch.py
@@ -1,2 +1,6 @@
 import requests, bs4 
-while True: print(bs4.BeautifulSoup(requests.get(f'https://www.ecs.soton.ac.uk/people/{input("Enter an ID: ")}').text, "lxml").head.find('meta', property="og:title")['content'])
+while True: 
+    try: 
+        print(bs4.BeautifulSoup(requests.get(f'https://www.ecs.soton.ac.uk/people/{input("Enter an ID: ")}').text, "lxml").head.find('meta', property="og:title")['content'])
+    except:
+        print("name cannot be found")
